b53dd6685e13899bb4f08b3ca138a67b37324f9b
Jennyfer2004 <jennifer.ss2004@gmail.com>
2025-03-13 01:14:25 -0400

    add the api model

---
+++ api.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+import joblib
+import numpy as np
+
+app = FastAPI()
+
+# Cargar el modelo guardado
+modelo = joblib.load("./modelo/modelo_xgboost.pkl")
+
+# Definir un esquema para la entrada
+class InputData(BaseModel):
+    data: list
+
+@app.post("/predict/")
+def predict(input_data: InputData):
+    X = np.array(input_data.data).reshape(1, -1)
+    prediction = modelo.predict(X)
+    return {"prediction": int(prediction[0])}
