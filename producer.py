bd81813458120b7f4199b40af2710e8af98ff6fd
mariandc18 <ma907222@gmail.com>
2025-03-13 05:48:36 -0400

    adding delay prediction in the producer

--- producer.py
+++ producer.py
@@ -3,6 +3,7 @@ import json
 import requests
 import time
 from concurrent.futures import ThreadPoolExecutor
+import requests  
 
 # Configuración del productor de Confluent Kafka
 producer_config = {
@@ -13,6 +14,7 @@ producer = Producer(producer_config)
 
 API_URL_STATE_VECTORS = 'https://opensky-network.org/api/states/all'
 API_URL_FLIGHTS = 'https://opensky-network.org/api/flights/all'
+API_URL_PREDICT = 'http://localhost:8000/predict'  
 
 
 def fetch_data():
@@ -34,8 +36,19 @@ def fetch_data():
     return None, None
 
 
+def get_delay_from_fastapi(flight_data):
+    """Obtiene la predicción del retraso desde el modelo FastAPI"""
+    try:
+        response = requests.post(API_URL_PREDICT, json=flight_data)
+        prediction = response.json()
+        return prediction.get('delay', 0)  # Retorna 0 si no se encuentra la predicción
+    except Exception as e:
+        print(f"[ERROR] Error al obtener la predicción: {e}")
+        return 0  # Valor por defecto si hay error
+
+
 def send_to_kafka(states_data, flights_data, topic='Air_Traffic'):
-    """Envía los datos de tráfico aéreo y vuelos a Kafka."""
+    """Envía los datos de tráfico aéreo y vuelos a Kafka, incluyendo la predicción de retraso"""
     if not states_data or "states" not in states_data:
         print(f"[WARNING] No hay datos de estado para enviar al tópico '{topic}'")
         return
@@ -50,6 +63,32 @@ def send_to_kafka(states_data, flights_data, topic='Air_Traffic'):
             flight_info.get("estDepartureAirport"),
             flight_info.get("estArrivalAirport")
         ])
+        
+        # Crear el diccionario con los datos que se enviarán
+        flight_data = {
+            'icao24': state[0],
+            'callsign': state[1],
+            'origin_country': state[2],
+            'time_position': state[3],
+            'last_contact': state[4],
+            'latitude': state[6],
+            'longitude': state[5],
+            'altitude': state[7],
+            'velocity': state[9],
+            'heading': state[10],
+            'vertical_rate': state[11],
+            'firstSeen': state[12],
+            'lastSeen': state[13],
+            'estDepartureAirport': state[14],
+            'estArrivalAirport': state[15]
+        }
+        
+        # Obtener la predicción de retraso desde el modelo de FastApi
+        delay = get_delay_from_fastapi(flight_data)
+        
+        # Agregar el campo 'delay' a los datos del vuelo
+        state.append(delay)
+        
         message = json.dumps(state).encode('utf-8')
         producer.produce(topic, value=message)
     
